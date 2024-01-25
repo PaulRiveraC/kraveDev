a24d78ea79b5c448dbf14836e81c10e80b455460
PaulRiveraC <paulriveracamacho@gmail.com>
2024-01-24 21:24:33 -0500

    FLOAT E INT INGRESADO OK

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -2,7 +2,6 @@ saludar=function(){
     let cmpImagenSaludo;
     cmpImagenSaludo=document.getElementById("imgSaludo");
     cmpImagenSaludo.src="./imagenes/giphy.gif";
-    console.log("ingresa a saludar")
-    
+    console.log("ingresa a saludar"); 
 
 }
--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -3,11 +3,30 @@ saludar=function(){
     let nombre=recuperarTexto("TxtNombre");
     //recuperar el valor de la caja de texto apellido
     let apellido=recuperarTexto("TxtApellido");
+    let edad=recuperarInt("TxtEdad");
+    let estatura=recuperarFloat("TxtEstatura");
 }
+
 recuperarTexto=function(idComponente){
     let componente;
     let valorIngresado;
-    componente=document.getElementById(idComponente);
+    componente= document.getElementById(idComponente);
     valorIngresado= componente.value;
     return valorIngresado;
+}
+
+recuperarInt= function(idComponente){
+    let valorcaja;
+    let valorEntero;
+    valorcaja= recuperarTexto(idComponente);
+    valorEntero= parseInt(valorcaja);
+    return valorEntero;
+}
+
+recuperarFloat= function(idComponente){
+    let valorcaja;
+    let valorflotante;
+    valorcaja= recuperarTexto(idComponente);
+    valorflotante= parseFloat(valorcaja);
+    return valorflotante;
 }
