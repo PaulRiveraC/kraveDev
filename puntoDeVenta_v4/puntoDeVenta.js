470965ebf55bc519ed11a93c0d86b16782839f4f
PaulRiveraC <paulriveracamacho@gmail.com>
2024-01-25 21:36:58 -0500

    Validacion completa

--- puntoDeVenta_v4/puntoDeVenta.js
+++ puntoDeVenta_v4/puntoDeVenta.js
@@ -30,6 +30,7 @@ calcularValorTotal = function(){
     //6. Invocar a calcularDescuento y lo que devuelve guardar en la variable valorDescuento
     valorDescuento=calcularValorDescuento(valorSubtotal,porcentajeDescuento);
     //7. Mostrar el resultado en el componente lblDescuento
+
     mostrarTexto("lblDescuento",valorDescuento);
         /*
             Caso de prueba: 
@@ -43,6 +44,7 @@ calcularValorTotal = function(){
 	//   El IVA debe calcularse sobre el valor del subtotal menos el descuento
     //9. Mostrar el resultado en el componente lblValorIVA  
     mostrarTexto("lblValorIVA",valorIVA);  
+
     /*
             Caso de prueba: 
                 precioProducto: 5.4  cantidad: 10 descuento: 10
@@ -56,6 +58,7 @@ calcularValorTotal = function(){
     valorTotal=calcularTotal(valorSubtotal,valorDescuento,valorIVA);
     //11. Mostrar el resultado en el componente lblTotal
     mostrarTexto("lblTotal",valorTotal);
+
      /*
             Caso de prueba: 
                 precioProducto: 5.4  cantidad: 10 descuento: 10
@@ -68,7 +71,7 @@ calcularValorTotal = function(){
     //12. Mostrar un resumen en el componente lblResumen, si no existe debe agregarlo
     
     mostrarTexto("lblResumen","valor a pagar por "+cantidad+ " "+nombreProducto+" con "+porcentajeDescuento+"% de descuento: "+valorTotal);
-        /*
+   /*
             Ejemplo: 
                 Valor a pagar por 20 cerveza corona con 10% de descuento: USD 48.75
             Si funciona, hacer un commit
@@ -76,6 +79,18 @@ calcularValorTotal = function(){
     
 }   
 limpiar=function(){
+    mostrarTexto("lblResumen",0.0);
+    mostrarTexto("lblTotal",0.0);
+    mostrarTexto("lblValorIVA",0.0);  
+    mostrarTexto("lblSubtotal",0.0);
+    mostrarTexto("lblDescuento",0.0);
+    mostrarTexto("lblTotal",0.0);
+
+    mostrarTextoEnCaja("txtProducto",0.0);
+    mostrarTextoEnCaja("txtCantidad",0.0);
+    mostrarTextoEnCaja("txtPrecio",0.0);  
+    mostrarTextoEnCaja("txtPorcentajeDescuento",0.0);
+    
     /*
         Dejar todas las cajas de texto con el valor cadena vacía, 0 ó 0.0 según el tipo de dato
         Dejar todos los textos de los montos con el valor 0.0
