29839ee3f1a587c840d58e70874a4733efab87be
PaulRiveraC <paulriveracamacho@gmail.com>
2024-02-03 23:40:56 -0500

    reto 34 corregido

--- puntoDeVentaV2/serviciosPuntoDeVenta.js
+++ puntoDeVentaV2/serviciosPuntoDeVenta.js
@@ -30,8 +30,10 @@ calcularTotal = function (subtotal, descuento, iva) {
 validarProducto = function (recuperar) {
     const letra2= recuperar.length;
     if (letra2>=0 && letra2<=10){
+        mostrarTexto("lblError1", "");
         mensaje=true;
     }else{
+        mostrarTexto("lblError1", "El nombre del producto no puede tener mas de 10 caracteres");
         mensaje=false;
     }
     return mensaje;
@@ -41,8 +43,10 @@ validarProducto = function (recuperar) {
 validarCantidad = function (recuperar) {
     let mensaje;
     if (recuperar>=0 & recuperar <=100){
+        mostrarTexto("lblError2", "");
         mensaje=true;
     }else{
+        mostrarTexto("lblError2", "La cantidad debe ser un numero entero entre 0 y 100");
         mensaje=false;
     }
     return mensaje;
@@ -51,8 +55,10 @@ validarCantidad = function (recuperar) {
 validarPrecio = function (recuperar) {
     let mensaje;
     if (recuperar>=0 & recuperar <=50){
+        mostrarTexto("lblError3", "");
         mensaje=true;
     }else{
+        mostrarTexto("lblError3", "El precio debe ser un numero entre 0 y 50");
         mensaje=false;
     }
     return mensaje;
