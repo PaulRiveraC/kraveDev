13d9475272ea6731054cd4d46d6fd5faa39a89f3
PaulRiveraC <paulriveracamacho@gmail.com>
2024-02-21 22:10:46 -0500

    paso 6 listo

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -76,14 +76,36 @@ ingresarLetra=function(){
     if(esMayuscula(recuperaLetra)){
         validar(recuperaLetra);
         if(coincidienciasGlobal===5){
-            mostrarImagen("ahorcadoImagen", "ganador.gif");
+            alert("HA GANADO");
         }
     }else{
         alert("SOLO SE ACEPTAN MAYUSCULAS")
     }
     intentosGlobal++;
     if(intentosGlobal===10){
-        mostrarImagen("ahorcadoImagen", "gameOver.gif");
+        alert("HA PERDIDO");
 
+    }
+}
+
+mostraAhorcado= function(){
+    if(errores==1){
+        mostrarImagen("ahorcadoImagen", "Ahorcado_01.png");
+    }else if(errores==2){
+        mostrarImagen("ahorcadoImagen", "Ahorcado_02.png");
+    }else if(errores==3){
+        mostrarImagen("ahorcadoImagen", "Ahorcado_03.png");
+    }else if(errores==4){
+        mostrarImagen("ahorcadoImagen", "Ahorcado_04.png");
+    }else if(errores==5){
+        mostrarImagen("ahorcadoImagen", "Ahorcado_05.png");
+    }else if(errores==6){
+        mostrarImagen("ahorcadoImagen", "Ahorcado_06.png");
+    }else if(errores==7){
+        mostrarImagen("ahorcadoImagen", "Ahorcado_07.png");
+    }else if(errores==8){
+        mostrarImagen("ahorcadoImagen", "Ahorcado_08.png");
+    }else if(errores==9){
+        mostrarImagen("ahorcadoImagen", "Ahorcado_09.png");
     }
 }
