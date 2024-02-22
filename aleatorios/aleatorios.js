6dd32712a2708bd3326e828070ad863c4fd7e57c
PaulRiveraC <paulriveracamacho@gmail.com>
2024-02-22 07:57:35 -0500

    cambios

--- aleatorios/aleatorios.js
+++ aleatorios/aleatorios.js
@@ -1,29 +1,34 @@
-aleatorios= function(){
-    min=1;
-    max=100;
+
+
+aleato1 = function () {
+    min = 1;
+    max = 100;
     min = Math.ceil(min);
     max = Math.floor(max);
-    let imprimir=Math.floor(Math.random() * (max - min + 1) + min);
-    console.log(imprimir);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-generarAleatorios =function(){
-    let aleatorios=[];
-    let inicio = aleatorios[0];
+generarAleatorios = function () {
+    let aleatorios = [];
     let numero = recuperarTexto("txtNumero");
-    let validar=false;
+    let validar = false;
     let imprimir;
-    if (numero >= 5){
-        if (numero <= 20){
-            validar=true;
-            for(let i=0; i<numero;i++){
-                let ale=aleatorios();
+    let imprimir2;
+    let ale;
+    let guardado;
+    let pantalla;
+    if (numero >= 5) {
+        if (numero <= 20) {
+            validar = true;
+            for (let i = 0; i < numero; i++) {
+                ale = aleato1();
+                aleatorios[i];
                 aleatorios.push[ale];
-                imprimir= console.log(i);
-                
+                imprimir = console.log(ale);
+                imprimir2 = console.log(aleatorios.push[ale]);
             }
-            let pantalla=mostrarResultados(aleatorios);
-        }else{
+            pantalla = mostrarResultados(guardado);
+        } else {
             validar;
         }
     } else {
@@ -32,17 +37,48 @@ generarAleatorios =function(){
 
 }
 
-mostrarResultados = function(arregloNumeros){
+
+
+mostraNota = function () {
     let cmpTabla = document.getElementById("divTable");
-    let contenidoTabla="<table><tr><th>VALOR</th></tr>";
-    let valor;
-    for(let i=0;i<notas.length;i++){
-        contenidoTabla+="<tr><td>";
-        contenidoTabla+=valor;
-        contenidoTabla+="</tr></td>"
+    let contenidoTabla = "<table><tr><th>NOTA</th></tr>";
+    let miNota;
+    for (let i = 0; i < notas.length; i++) {
+        miNota = notas[i]
+        contenidoTabla += "<tr><td>";
+        contenidoTabla += miNota;
+        contenidoTabla += "</tr></td>"
     }
-    contenidoTabla+="</table>"
-    cmpTabla.innerHTML=contenidoTabla;
-    
+    contenidoTabla += "</table>"
+    cmpTabla.innerHTML = contenidoTabla;
 }
 
+mostrarResultados = function (arreglonumeros) {
+    let cmpTabla = document.getElementById("divTable");
+    let contenidoTabla = "<table><tr><th>NOTA</th></tr>";
+    let miNota;
+    for (let i = 0; i < arreglonumeros.length; i++) {
+        miNota = arreglonumeros[i];
+        contenidoTabla += "<tr><td>";
+        contenidoTabla += miNota;
+        contenidoTabla += "</tr></td>"
+    }
+    contenidoTabla += "</table>";
+    cmpTabla.innerHTML = contenidoTabla;
+}
+
+
+
+mostraNota = function () {
+    let cmpTabla = document.getElementById("divTable");
+    let contenidoTabla = "<table><tr><th>NOTA</th></tr>";
+    let miNota
+    for (let i = 0; i < notas.length; i++) {
+        miNota = notas[i];
+        contenidoTabla += "<tr><td>";
+        contenidoTabla += miNota;
+        contenidoTabla += "</tr></td>"
+    }
+    contenidoTabla += "</table>"
+    cmpTabla.innerHTML = contenidoTabla;
+}
