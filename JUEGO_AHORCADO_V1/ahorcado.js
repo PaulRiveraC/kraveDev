e4eb6c739b349bbe9ad0c0f96d6c25107114c665
PaulRiveraC <paulriveracamacho@gmail.com>
2024-02-21 22:44:41 -0500

    reto 41

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,14 +1,14 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
 let palabraSecretaGlobal;
-let intentosGlobal=0;
-let coincidienciasGlobal=0;
-let errores=0;
+let intentosGlobal = 0;
+let coincidienciasGlobal = 0;
+let errores = 0;
 
 esMayuscula = function (caracter) {
     let codigoAscii = caracter.charCodeAt(0);
-    if(codigoAscii >= 65 && codigoAscii <= 90){
+    if (codigoAscii >= 65 && codigoAscii <= 90) {
         return true
-    } else{
+    } else {
         return false
     }
 }
@@ -17,24 +17,24 @@ guardarPalabra = function () {
     let palabra;
     let longitud;
     let letra;
-    let contador=false;
+    let contador = false;
 
     palabra = recuperarTexto("txtSecreta");
     longitud = palabra.length;
 
-    for(let i=0;i<palabra.length;i++){
-        letra=palabra.charAt(i);
-        if(esMayuscula(letra)){
-            contador=true;
+    for (let i = 0; i < palabra.length; i++) {
+        letra = palabra.charAt(i);
+        if (esMayuscula(letra)) {
+            contador = true;
         }
     }
-    
+
     if (longitud != 5) {
         mostrarTexto("lblErrores", "Debe tener 5 Caracteres");
-    }else if(contador == false){
+    } else if (contador == false) {
         mostrarTexto("lblErrores", "Todas las letras deben ser Mayusculas");
-    }else {
-        palabraSecretaGlobal=palabra;
+    } else {
+        palabraSecretaGlobal = palabra;
         console.log(palabraSecretaGlobal);
     }
 }
@@ -53,59 +53,59 @@ mostrarLetra = function (letra, posicion) {
     }
 }
 
-validar=function(letra){
-    let letrasEncontradas=0;
-    for(let i=0;i<palabraSecretaGlobal.length;i++){
-        if(palabraSecretaGlobal.charAt(i)===letra){
+validar = function (letra) {
+    let letrasEncontradas = 0;
+    for (let i = 0; i < palabraSecretaGlobal.length; i++) {
+        if (palabraSecretaGlobal.charAt(i) === letra) {
             mostrarLetra(letra, i)
             letrasEncontradas++
         }
     }
-    if(letrasEncontradas==0){
+    if (letrasEncontradas == 0) {
         alert("LA LETRA NO ES PARTE DE LA PALABRA");
         errores++;
         mostraAhorcado();
-    } else{
+    } else {
         coincidienciasGlobal++;
     }
 }
 
-ingresarLetra=function(){
+ingresarLetra = function () {
     let recuperaLetra;
-    recuperaLetra=recuperarTexto("txtLetra");
-    if(esMayuscula(recuperaLetra)){
+    recuperaLetra = recuperarTexto("txtLetra");
+    if (esMayuscula(recuperaLetra)) {
         validar(recuperaLetra);
-        if(coincidienciasGlobal===5){
+        if (coincidienciasGlobal === 5) {
             mostrarImagen("ahorcadoImagen", "ganador.gif");
         }
-    }else{
+    } else {
         alert("SOLO SE ACEPTAN MAYUSCULAS")
     }
     intentosGlobal++;
-    if(intentosGlobal===10){
+    if (intentosGlobal === 10) {
         mostrarImagen("ahorcadoImagen", "gameOver.gif");
 
     }
 }
 
-mostraAhorcado= function(){
-    if(errores==1){
+mostraAhorcado = function () {
+    if (errores == 1) {
         mostrarImagen("ahorcadoImagen", "Ahorcado_01.png");
-    }else if(errores==2){
+    } else if (errores == 2) {
         mostrarImagen("ahorcadoImagen", "Ahorcado_02.png");
-    }else if(errores==3){
+    } else if (errores == 3) {
         mostrarImagen("ahorcadoImagen", "Ahorcado_03.png");
-    }else if(errores==4){
+    } else if (errores == 4) {
         mostrarImagen("ahorcadoImagen", "Ahorcado_04.png");
-    }else if(errores==5){
+    } else if (errores == 5) {
         mostrarImagen("ahorcadoImagen", "Ahorcado_05.png");
-    }else if(errores==6){
+    } else if (errores == 6) {
         mostrarImagen("ahorcadoImagen", "Ahorcado_06.png");
-    }else if(errores==7){
+    } else if (errores == 7) {
         mostrarImagen("ahorcadoImagen", "Ahorcado_07.png");
-    }else if(errores==8){
+    } else if (errores == 8) {
         mostrarImagen("ahorcadoImagen", "Ahorcado_08.png");
-    }else if(errores==9){
+    } else if (errores == 9) {
         mostrarImagen("ahorcadoImagen", "Ahorcado_09.png");
     }
 }
---
+++ arreglos/ejercicio1.js
@@ -0,0 +1,44 @@
+let notas = [];
+
+agregarElementos = function () {
+    notas.push(5);;
+    notas.push(10);
+    console.log(notas.length);
+}
+
+recorerAreglo = function () {
+    let notaR
+    for (let indice = 0; indice < notas.length; indice++) {
+        notaR = notas[indice];
+        console.log(notaR);
+    }
+}
+
+probarAgregar = function () {
+    let notaRecuperada;
+    notaRecuperada = recuperarInt("txtNota");
+    agregarNota(notaRecuperada);
+}
+
+agregarNota = function (nota) {
+    notas.push(nota);
+    mostraNota();
+}
+
+calcularPromedio = function () {
+    let sumaNotas = 0;
+    let promedio;
+    for (let i = 0; i < notas.length; i++) {
+        sumaNotas = sumaNotas + notas[i];
+    }
+    promedio = sumaNotas / notas.length;
+    return promedio;
+}
+
+ejecutarPromedio = function () {
+    let invocar;
+    invocar = calcularPromedio();
+    mostrarTexto("lblPromedio", invocar);
+
+}
+//ejemplos de clase
