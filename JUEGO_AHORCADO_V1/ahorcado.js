0278681fe4cf305fb413f3c0f98d9ba04f29d8a9
PaulRiveraC <paulriveracamacho@gmail.com>
2024-02-21 21:46:00 -0500

    paso 1

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -8,3 +8,29 @@ esMayuscula = function (caracter) {
         return false
     }
 }
+
+guardarPalabra = function () {
+    let palabra;
+    let longitud;
+    let letra;
+    let contador=false;
+
+    palabra = recuperarTexto("txtSecreta");
+    longitud = palabra.length;
+
+    for(let i=0;i<palabra.length;i++){
+        letra=palabra.charAt(i);
+        if(esMayuscula(letra)){
+            contador=true;
+        }
+    }
+    
+    if (longitud != 5) {
+        mostrarTexto("lblErrores", "Debe tener 5 Caracteres");
+    }else if(contador == false){
+        mostrarTexto("lblErrores", "Todas las letras deben ser Mayusculas");
+    }else {
+        palabraSecretaGlobal=palabra;
+        console.log(palabraSecretaGlobal);
+    }
+}
