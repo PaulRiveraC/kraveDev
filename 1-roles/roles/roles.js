50b551e7d1c24b3fbc585660efe08f256f83cba5
PaulRiveraC <paulriveracamacho@gmail.com>
2024-02-22 23:22:12 -0500

    reto 46 opcion a

--- 1-roles/roles/roles.js
+++ 1-roles/roles/roles.js
@@ -1,7 +1,9 @@
 let empleados = [
     {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
-    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
+    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0},
+    { cedula: "0302578166", nombre: "Francisco", apellido: "Peres", sueldo: 600.0}
 ]
+
 
 mostrarOpcionEmpleado = function () {
     let mostrar;
@@ -27,3 +29,24 @@ mostrarOpcionResumen = function () {
     mostrarTotales();
 }
 
+mostrarEmpleado = function () {
+    let cmpTabla = document.getElementById("tablaEmpleados");
+    let contenidoTabla = "<table><tr>" +
+        "<th>CEDULA</th>" +
+        "<th>NOMBRE</th>" +
+        "<th>APELLIDO</th>" +
+        "<th>SUELDO</th>" +
+        "</tr>";
+    let elementoEmpleado;
+    for (let i = 0; i < empleados.length; i++) {
+        elementoEmpleado = empleados[i]
+        contenidoTabla +=
+            "<tr><td>" + elementoEmpleado.cedula + "</td>"
+            + "<td>" + elementoEmpleado.nombre + "</td>"
+            + "<td>" + elementoEmpleado.apellido + "</td>"
+            + "<td>" + elementoEmpleado.sueldo + "</td>"
+            + "</tr>"
+    }
+    contenidoTabla += "</table>"
+    cmpTabla.innerHTML = contenidoTabla;
+}
